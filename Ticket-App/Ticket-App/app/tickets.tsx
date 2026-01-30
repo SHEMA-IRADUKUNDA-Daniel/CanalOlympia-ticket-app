00c893829eb28b86391bf21cabd55b1c14b50480
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2026-01-30 20:34:13 +0200

    separating ticket categories

--- Ticket-App/Ticket-App/app/tickets.tsx
+++ Ticket-App/Ticket-App/app/tickets.tsx
@@ -1,11 +1,12 @@
-import { View, Switch, StatusBar, Text, TouchableOpacity } from "react-native";
+import { View, StatusBar, Text, TouchableOpacity } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { NavigationBar } from "@/components/navBar";
-import { Feather } from "@expo/vector-icons";
+
 import { useState } from "react";
+import Ticket from "@/components/ticket";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tickets = () => {
-  const [showDeleteMenu, setShowDeleteMenu] = useState(false);
   const [activeTab, setActiveTab] = useState("upcoming");
 
   return (
@@ -39,61 +40,28 @@ const Tickets = () => {
             </TouchableOpacity>
           ))}
         </View>
-        <LinearGradient
-          colors={["#000", "#BC002D"]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 0 }}
-          className="w-150 h-40 top-10 rounded-2xl"
-        >
-          <View className="flex-row p-5 justify-between">
-            <View>
-              <Text className="text-white mb-2 font-poppins-bold text-xs">
-                Avatar: Fire and ash
-              </Text>
-              <Text className="text-gray-300 font-roboto-medium text-xs">
-                Thursday 18,December 2025 8:00 PM
-              </Text>
-            </View>
-            <TouchableOpacity
-              onPress={() => setShowDeleteMenu(!showDeleteMenu)}
-            >
-              <Feather name="more-vertical" size={24} color="white" />
-            </TouchableOpacity>
+        {activeTab === "upcoming" && (
+          <View className="mt-6 space-y-4">
+            <Ticket />
           </View>
-          {showDeleteMenu && (
-            <TouchableOpacity className="absolute top-12 right-0 bg-primary/50 p-4 rounded-full transition-all shadow-lg shadow-black z-10">
-              <Text className="text-white font-poppins-bold text-xs ">
-                Delete ticket
-              </Text>
-            </TouchableOpacity>
-          )}
-          <View className="flex-row px-5 mt-5 justify-between w-9/12">
-            <View className="flex-1 justify-start items-start">
-              <Text className="text-gray-300 font-poppins-semibold text-xs">
-                Tickets
-              </Text>
-              <Text className="text-white font-poppins-semibold text-xs">
-                2
-              </Text>
-            </View>
-            <View className="flex-col justify-start  items-center">
-              <Text className="text-gray-300 font-poppins-semibold text-xs">
-                Seat
-              </Text>
-              <Text className="text-white font-poppins-semibold text-xs ml-10 mr-9">
-                D7,D8
-              </Text>
-            </View>
-            <View>
-              <Text className="text-gray-300 font-poppins-semibold text-xs">
-                Total
-              </Text>
-              <Text className="text-white font-poppins-semibold text-xs">
-                12,000 Rwf
-              </Text>
-            </View>
+        )}
+
+        {activeTab === "reserved" && (
+          <View className="mt-20 space-y-4 items-center justify-center">
+            <Ionicons name="ticket" size={65} color="white" />
+            <Text className="font-poppins-bold text-sm text-white/70">
+              You don&apos;t have any reserved tickets yet.
+            </Text>
           </View>
-        </LinearGradient>
+        )}
+        {activeTab === "past" && (
+          <View className="mt-20 space-y-4 items-center justify-center">
+            <Ionicons name="ticket" size={65} color="white" />
+            <Text className="font-poppins-bold text-sm text-white/70">
+              You don&apos;t have any past tickets yet.
+            </Text>
+          </View>
+        )}
       </View>
       <View className="flex-1 justify-end ">
         <NavigationBar />
