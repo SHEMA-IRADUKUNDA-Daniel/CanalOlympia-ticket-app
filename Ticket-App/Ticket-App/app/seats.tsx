2e0dc492ec4d17ab0d6f87a7af6eac1e97af8ab5
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2026-02-12 22:38:08 +0200

    adding temporary dummy logic and data to make payment functionality work

--- Ticket-App/Ticket-App/app/seats.tsx
+++ Ticket-App/Ticket-App/app/seats.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Animated } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { Feather } from "@expo/vector-icons";
 import Seat from "@/components/seat";
@@ -6,13 +6,19 @@ import Sofa from "@/components/sofa";
 import BackButton from "@/components/backButton";
 import LinearBackground from "@/components/linearBackground";
 import { seatType } from "@/interface";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Button from "@/components/button";
 import { useRouter } from "expo-router";
+import Screen from "@/components/screen";
 
 const Seats = () => {
+  const rotateAnim = useRef(new Animated.Value(0)).current;
+  const [rotated, setRotated] = useState(false);
+
+  const paymentAmount = 3000;
   const route = useRouter();
   const [selectedSeats, setSelectedSeats] = useState<seatType[]>([]);
+  const totalAmount = paymentAmount * selectedSeats.length;
 
   const handleSeatPress = ({ id, number, isAvailable }: seatType) => {
     if (!isAvailable) return;
@@ -35,30 +41,40 @@ const Seats = () => {
         <BackButton onPress={() => route.push("/")} />
         <TouchableOpacity
           onPress={() => {
+            if (selectedSeats.length === 0) return;
+            Animated.timing(rotateAnim, {
+              toValue: rotated ? 0 : 1,
+              duration: 1000,
+              useNativeDriver: true,
+            }).start();
+
+            setRotated(!rotated);
             setTimeout(() => {
               setSelectedSeats([]);
             }, 2000);
           }}
         >
-          <Feather name="refresh-cw" size={26} color="white" />
+          <Animated.View
+            style={{
+              transform: [
+                {
+                  rotate: rotateAnim.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: ["0deg", "180deg"],
+                  }),
+                },
+              ],
+            }}
+          >
+            <Feather name="refresh-cw" size={26} color="white" />
+          </Animated.View>
         </TouchableOpacity>
       </View>
       <Text className="text-white text-center mt-10 font-poppins-bold text-sm">
         Screen
       </Text>
       <View className="mt-5 h-1 w-[80%] self-center bg-secondary rounded-full shadow-lg  " />
-      <LinearGradient
-        colors={["rgba(188,0,46,0.6)", "transparent"]}
-        start={{ x: 0.5, y: 0 }}
-        end={{ x: 0.5, y: 1 }}
-        style={{
-          width: "80%",
-          height: 80,
-          marginTop: -2,
-          opacity: 10,
-        }}
-        className="self-center rounded-2xl shadow-secondary shadow-lg"
-      />
+      <Screen />
       <Seat onSeatPress={handleSeatPress} selectedSeats={selectedSeats} />
 
       <View className="mt-10 px-5 flex-row items-center justify-between ">
@@ -66,19 +82,25 @@ const Seats = () => {
         <Sofa text={"Not available"} color="#767577" />
         <Sofa text={"Selected"} color="#BC002E" />
       </View>
-      <View className="flex-row  justify-between items-center space-x-1 px-3 mt-10">
-        <View className="flex-col items-center bg-primary p-5 rounded-3xl">
-          <Text className="text-white">Dec 19,2025</Text>
-          <Text className="text-white">8:PM</Text>
+      <View className="flex-row  justify-between items-center  px-3 mt-10">
+        <View className="flex-col items-center bg-primary p-4 rounded-2xl">
+          <Text className="text-[#767577] font-roboto-medium text-xs mb-2">
+            Dec 19,2025
+          </Text>
+          <Text className="text-white font-roboto-bold text-sm">8:PM</Text>
         </View>
-        <View className="bg-secondary p-7 rounded-3xl">
-          <Text className="text-white">Premiere</Text>
+        <View className="bg-secondary flex-col items-center p-4 rounded-2xl">
+          <Text className="text-white/80  font-roboto-medium text-xs mb-2">
+            Category
+          </Text>
+          <Text className="text-white font-roboto-bold text-sm">Premiere</Text>
         </View>
-        <View className="flex-col flex-wrap items-center bg-primary p-5 rounded-3xl">
-          <Text className="text-white">Seats</Text>
-          <View>
+        <View className="flex-col flex-wrap items-center bg-primary p-4 rounded-2xl">
+          <Text className="text-[#767577] font-roboto-medium text-xs mb-2">
+            Selected seats
+          </Text>
+          <View className="flex-row items-center gap-1">
             <Sofa
-              className=""
               text={selectedSeats.map((s) => s.number).join(", ")}
               color="#BC002E"
             />
@@ -87,7 +109,7 @@ const Seats = () => {
       </View>
       {selectedSeats.length > 0 && (
         <View className="px-3">
-          <Button name="Pay" text="12000" />
+          <Button name="Pay" amount={totalAmount} />
         </View>
       )}
     </View>
--- Ticket-App/Ticket-App/components/button.tsx
+++ Ticket-App/Ticket-App/components/button.tsx
@@ -1,18 +1,20 @@
 import { ButtonProps } from "@/interface";
 import { Text, TouchableOpacity } from "react-native";
 
-const Button = ({ name, text, onPress, className }: ButtonProps) => {
+const Button = ({ name, amount, onPress, className }: ButtonProps) => {
   return (
     <TouchableOpacity
       onPress={onPress}
       className={
         className
           ? className
-          : "flex-row justify-between  p-5 bg-secondary rounded-3xl w-full self-center mt-10"
+          : "flex-row justify-between  p-5 bg-secondary rounded-2xl w-full self-center mt-10"
       }
     >
       <Text className="text-white font-poppins-bold text-base">{name}</Text>
-      <Text className="text-white font-poppins-bold text-base">{text} Rwf</Text>
+      <Text className="text-white font-poppins-bold text-base">
+        Total: {amount} Rwf
+      </Text>
     </TouchableOpacity>
   );
 };
